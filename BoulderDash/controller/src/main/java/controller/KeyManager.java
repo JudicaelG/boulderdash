fb96ecda164030bfd2bc795a0df765d2a10af09d
[JudicaelGuillaume] <[judicael.guillaume@viacesi.fr]>
2017-06-22 10:32:57 +0200

    Delete BoulderDashController and add KeyManager.java

---
+++ BoulderDash/controller/src/main/java/controller/KeyManager.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.Set;
+
+public class KeyManager implements KeyListener {
+    // ATTRIBUTES
+    private boolean[] keys;
+    public boolean up, down, left, right;
+    private final Set<Character> pressed = new HashSet<Character>();
+
+    // CONSTRUCTOR
+    public KeyManager() {
+        keys = new boolean[256];
+    }
+
+    public void tick() {
+        up = keys[KeyEvent.VK_UP];
+        down = keys[KeyEvent.VK_DOWN];
+        left = keys[KeyEvent.VK_LEFT];
+        right = keys[KeyEvent.VK_RIGHT];
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+    }
+}
